2847af2503d26c60b4205e9f12bde0f7e8c4cab9
Ulysses Souza <usouza@axway.com>
2016-07-22 11:22:11 +0200

    Create .ppm file in tmp dir and remove it after encoding with ffmpeg

--- gource-video-maker.sh
+++ gource-video-maker.sh
@@ -1,2 +1,3 @@
-gource -1280x720 -s 1 --user-image-dir ~/Dropbox/Photos -o $1.ppm
-ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i $1.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 $1.mp4
+gource -1280x720 -s 1 --user-image-dir ~/Dropbox/Photos -o /tmp/$1.ppm
+ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i /tmp/$1.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 $1.mp4
+rm -rf /tmp/$1.ppm
